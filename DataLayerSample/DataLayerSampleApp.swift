6c426e640074533656e09369daf797ca8107b696
wdesimini <wdesimini8@gmail.com>
2021-12-01 19:01:55 -0800

    Initial Commit

---
+++ DataLayerSample/DataLayerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataLayerSampleApp.swift
+//  DataLayerSample
+//
+//  Created by Wilson Desimini on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct DataLayerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
