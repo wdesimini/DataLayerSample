6d717d9f714b682920ad9cd02ab89cc7b2bcaecf
wdesimini <wdesimini8@gmail.com>
2021-12-09 15:38:53 -0800

    add data manager to app file

--- DataLayerSample/Shared/App/DataLayerSampleApp.swift
+++ DataLayerSample/Shared/App/DataLayerSampleApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct DataLayerSampleApp: App {
+    private let data = DataManager.shared
+    
     var body: some Scene {
         WindowGroup {
-            ContentView(viewModel: ContentViewModel(contentId: UUID(), data: .shared))
+            if let viewModel = contentViewModel {
+                ContentView(viewModel: viewModel)
+            } else {
+                Text("(no content)")
+            }
         }
+    }
+    
+    var contentViewModel: ContentViewModel? {
+        let contents = data.contentData.objectsById.values
+        guard let contentId = contents.first?.id else { return nil }
+        return ContentViewModel(contentId: contentId, data: data)
     }
 }
