9ee9206417937ea9c575cc2731a2a5b786722c08
Wilson Desimini <wdesimini8@gmail.com>
2022-01-09 09:54:45 -0800

    refactor unit tests

--- DataLayerSampleTests/DataLayerSampleTests.swift
+++ DataLayerSampleTests/DataLayerSampleTests.swift
@@ -10,6 +10,7 @@ import XCTest
 
 class DataLayerSampleTests: XCTestCase {
     private let data = DataManager.shared
+    private var content: Content?
 
     override func setUpWithError() throws {
         try data.register()
@@ -17,25 +18,30 @@ class DataLayerSampleTests: XCTestCase {
 
     override func tearDownWithError() throws {
         try data.reset()
+        content = nil
     }
     
     func testCreateContent() throws {
-        let data = self.data.contentData
-        let content = try createSampleContent(data: data)
-        let stored = data.read(objectWithId: content.id)
+        // create
+        let service = data.contentData
+        let content = Content(id: UUID(), text: "What da dog doin?")
+        service.create(content)
+        self.content = content
+        
+        // check content created
+        let stored = service.read(objectWithId: content.id)
         XCTAssertNotNil(stored)
     }
     
     func testDeleteContent() throws {
         // create
-        let data = self.data.contentData
-        let content = try createSampleContent(data: data)
-        let stored = data.read(objectWithId: content.id)
-        XCTAssertNotNil(stored)
+        try testCreateContent()
+        XCTAssertNotNil(content)
         
         // delete
-        data.delete(content)
-        let deleted = data.read(objectWithId: content.id)
+        let service = data.contentData
+        service.delete(content!)
+        let deleted = service.read(objectWithId: content!.id)
         XCTAssertNil(deleted)
     }
 
@@ -51,18 +57,5 @@ class DataLayerSampleTests: XCTestCase {
         let mockContent = try JSONDecoder().decode(Content.self, from: mock)
         XCTAssertTrue(mockContent.id == sample!.id)
         XCTAssertTrue(mockContent.text == sample!.text)
-    }
-}
-
-// MARK: Utility
-
-extension DataLayerSampleTests {
-    private func createSampleContent(data: DataService<Content>) throws -> Content {
-        let idString = "70881D22-E283-42BF-809B-656D8D57518C"
-        let contentId = UUID(uuidString: idString)!
-        let text = "What da dog doin?"
-        let content = Content(id: contentId, text: text)
-        data.create(content)
-        return content
     }
 }
