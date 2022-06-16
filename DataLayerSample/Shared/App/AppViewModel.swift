bb0815fe86408b5ae9d51a0a6c38ee399086965e
Wilson Desimini <wdesimini8@gmail.com>
2022-06-15 17:54:01 -0700

    debug view-model malloc

---
+++ DataLayerSample/Shared/App/AppViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  AppViewModel.swift
+//  DataLayerSample
+//
+//  Created by Wilson Desimini on 6/15/22.
+//
+
+import Foundation
+
+#if DEBUG
+class AppViewModel: DebugClass {}
+#else
+class AppViewModel {}
+#endif
+
+class DebugClass {
+    static var instanceCountByTypeDescription = [String: Int]()
+    static var typeDescription: String {
+        .init(describing: Self.self)
+    }
+
+    init() {
+        let typeDescription = Self.typeDescription
+        Self.instanceCountByTypeDescription[typeDescription, default: 0] += 1
+        let instanceCount = Self.instanceCountByTypeDescription[typeDescription, default: 0]
+        print("\(typeDescription) init - \(instanceCount)")
+    }
+
+    deinit {
+        let typeDescription = Self.typeDescription
+        Self.instanceCountByTypeDescription[typeDescription, default: 0] -= 1
+        let instanceCount = Self.instanceCountByTypeDescription[typeDescription, default: 0]
+        print("\(typeDescription) deinit - \(instanceCount)")
+    }
+}
